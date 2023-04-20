1beacfa4239a65564e76871f5d18d425006771f8
phenyo-droid <pnkoana61@gmail.com>
2023-04-20 13:30:43 +0200

    almost through creating the base doodler game

---
+++ app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+    let isGameOver = false
+    let platformCount = 5
+
+    function createDoodler(){
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace +'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+    }
+    
+    class Platform{
+        constructor()
+    }
+
+    function createPlatforms(){
+        for(let i = 0; i < platformCount; i++){
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform
+        }
+    }
+
+    function start(){
+        if(!isGameOver == false){
+            createDoodler()
+            createPlatforms()
+        }
+    }
+})
